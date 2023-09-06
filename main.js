827990aa53ab29dd3b1baf01a52f5a9ba048c90d
Olladi <aleksandr.zuev01@mail.ru>
2023-09-06 11:39:04 +0300

    Шестой коммит

---
+++ main.js
@@ -0,0 +1,81 @@
+
+
+     // дз5
+
+//  задание 1
+
+// let a=0;
+// while(a<=50){
+//     if(a!=0){
+//     console.log(a);}
+//     a++;
+// }
+
+// задание 2
+
+// let a=50;
+// do{
+//     console.log(a);
+//     --a;}
+//     while(a>=25);
+
+
+// задание 3
+
+// for (let a=2500; a<=3000; a++){
+//     if(a%2==0)continue;
+//     console.log(a);
+ // }
+
+ // дз №6
+ // задание 1
+  let a = 1
+  let b = prompt('укажите число')
+  switch(true){
+    case a==b:
+    alert("верно")
+    break;
+    case a>b:
+    alert("неверно")
+    break;
+    case a<b:
+    alert("неверно")
+    break
+    default:
+    alert("не сработало")
+ }
+
+
+// задание 2
+//let a = prompt('укажите число от 1 до 12')
+//switch(a){
+//    case "1":
+//    case "2":
+//    case "3":
+//    alert("Зима")
+//    break;
+//    case "4":
+//    case "5":
+//    case "6":
+//    alert("Весна")
+//    break;
+//    case "7":
+//    case "8":
+//    case "9":
+//    alert("Лето")
+//    break;
+//    case "10":
+//    case "11":
+//    case "12":
+//    alert("Осень")
+//    break;
+//    default:
+//    alert("Ошибка! Напишите число от 1 до 12")
+// }
+
+
+// задание 3
+// let firstname = "Marat";
+// let lastname = "Monasypov";
+// let nickname = null;
+// alert(firstname ?? lastname ?? nickname) 
